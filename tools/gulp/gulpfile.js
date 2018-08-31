77fcc85ddb169f5e5f3ec97975f57d7f7a856dae
Anton Maisak <Majsak.AV@esphere.ru>
2018-08-31 18:56:24 +0300

    @

--- tools/gulp/gulpfile.js
+++ tools/gulp/gulpfile.js
@@ -1,5 +1,48 @@
 var gulp = require('gulp');
+var fs = require('fs');
+var glob = require('glob');
+const srcDest = '../../src/scss/palettes/*.scss';
+const releaseDest = '../../dist/css/';
 
-console.log('\x1Bc');
+function clear() {
+	console.log('\x1Bc');
+}
 
-// gulp.task('default', [ 't1', 't2', 't3' ]);
+function parseFile (f , onReadSuccess) {
+	fs.readFile(f, 'utf8', (err, data) => {
+		if (err) throw err;
+		onReadSuccess (data.toString().split("\n"));
+	  });
+}
+
+function parseLine (line) {
+	var keyValuePair = line.split('$')[1].split(':');
+	var key = keyValuePair[0];
+	var value = keyValuePair[1].replace(';','');
+	return {
+		key: key,
+		value:value
+	};
+}
+
+let d = [];
+
+gulp.task('default', function() {
+	clear();
+	return glob(srcDest, function (e, files ) {
+
+		files.forEach ( f => {
+
+			parseFile ( f , function ( data) {
+				data.forEach ( d => {
+					var s = parseLine( d );
+					d.push(s);
+					console.log (d.length);
+				})
+
+			});
+
+		});
+
+	});
+});
